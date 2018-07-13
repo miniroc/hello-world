07583a7330ad48e29b1473895181c3324db8db00
HappyNest <xiaopengliu2000@hotmail.com>
2018-07-12 22:52:36 -0700

    add copy backup files

--- file_manipulate.py
+++ file_manipulate.py
@@ -1,6 +1,9 @@
 '''
 '''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
+import shutil
 
 def remove_duplicate_files(root_dir):
     #creating an empty dictionary or use "di = dict()" to creat an empty dictionary
@@ -70,14 +73,75 @@ def printfilenames(root, extension):
 #                     print("ffmpeg -i \"{0}\" -vn -acodec copy \"{1}.mp3\"".format(f, fname))
                     outf.write("ffmpeg -i \"{0}\" -vn -acodec libmp3lame \"{1}.mp3\"\n".format(f, fname))
     
+def recursiveTraverse(RootDir):
+    for lists in os.listdir(RootDir):
+        path = os.path.join(RootDir, lists)
+        print(path)
+        if os.path.isdir(path):
+            recursiveTraverse(path)
+
+
+def copyDiffFiles(srcRootDir, destRootDir):
+    srcf={}
+    srcfn = 0
+    desf={}
+    desfn = 0
+#     record folders and files, record folders first for later folder creation before file inside copy failure
+    for srcrootdir, srcdir, srcfiles in os.walk(srcRootDir):
+        for folder in srcdir:
+            fullpath = os.path.join(srcrootdir, folder)
+            srcfn += 1
+            srcf[srcfn] = fullpath
+            print(fullpath)       
+        for f in srcfiles:
+            fullpath = os.path.join(srcrootdir, f)
+            srcfn += 1
+            srcf[srcfn] = fullpath
+            print(fullpath)
+    print("src file numbers", srcfn)        
+
+#     print("des files:")        
+    for desrootdir, desdir, desfiles in os.walk(destRootDir):
+        for folder in desdir:
+            fullpath = os.path.join(srcrootdir, folder)
+            desfn += 1
+            desf[desfn] = fullpath
+            print(fullpath)
+        for f in desfiles:
+            fullpath = os.path.join(desrootdir, f)
+            desfn += 1
+            desf[fullpath] = desfn
+            print(fullpath)
+    print("des file numbers", desfn) 
+    
+    i = 1    
+    while(i <= srcfn):
+#         print("before replace: ", srcf[i])
+        des = srcf[i].replace(srcRootDir, destRootDir)
+#         print("after replace: ", srcf[i])
+
+#         check if this file already exits, check if key exist need to use in
+        if des in desf:
+            i += 1
+            continue
+        
+#         print("des file: ", des)
+        if os.path.isdir(srcf[i]):
+            if not os.path.exists(des):
+                os.makedirs(des)
+        else:
+            shutil.copyfile(srcf[i], des)
+#           shutil.copy(srcf[i], des)
+            print("copied ", srcf[i])
+        i += 1 
 
 def main():
-    root_dir = "E:\\5.Ethan\\English Video\\new"
-    print(root_dir)
+#     root_dir = "E:\\5.Ethan\\English Video\\new"
+#     print(root_dir)
 #     remove_duplicate_files(root_dir)
 #     printfilenames(root_dir, "mkv")
-    rename_files(root_dir)
-
+#     rename_files(root_dir)
+    copyDiffFiles("F:\\Coursera","F:\\New folder")
 
 if __name__ == "__main__":
     main()
